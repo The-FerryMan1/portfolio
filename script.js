840c924cf10f9bb0d494da5909db5c29b80d1f39
The-FerryMan1 <mercedroxy@gmail.com>
2024-09-01 14:52:07 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const goToTop = document.getElementById('goToTop');
+const main = document.getElementById('main');
+
+goToTop.addEventListener('click', ()=>{
+    main.scrollTop = 0
+})
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js, ./src/*}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Lato':['Lato', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
